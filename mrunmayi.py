d365b68eb28f99d286aaa9c7dc8205abf41ed048
pvgneha <44013712+pvgneha@users.noreply.github.com>
2018-10-17 07:34:59 +0530

    Add files via upload

---
+++ mrunmayi.py
@@ -0,0 +1,4 @@
+sum=0
+for x in range(2,100,2):
+    sum=sum+x
+print("the sum is",sum)
---
+++ siddhi.py
@@ -0,0 +1,13 @@
+Python 3.6.2 (v3.6.2:5fd33b5, Jul  8 2017, 04:14:34) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print("hello to the world of program")
+hello to the world of program
+>>> print("Siddhi Ashtekar")
+Siddhi Ashtekar
+>>> print("fycs")
+fycs
+>>> print("2018-2019")
+2018-2019
+>>> print(18303)
+18303
+>>> 
